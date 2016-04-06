37ae37e2d731e56d03e54aa873b8f84341bba588
devsden2015 <devsden2015@gmail.com>
2016-04-07 01:02:28 +0530

    First API Call

---
+++ InventoryManagement.API/Controllers/EmployeeAPIController.cs
@@ -0,0 +1,32 @@
+﻿using InventoryManagement.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Microsoft.ApplicationBlocks.Data;
+using System.Configuration;
+
+namespace InventoryManagement.API.Controllers
+{
+    public class EmployeeAPIController : ApiController
+    {
+        public void GetEmployee(long EmpId = 0)
+        {
+
+
+            SqlParameter param = new SqlParameter();
+            //param.ParameterName = "@Id";
+            //param.SqlDbType = SqlDbType.Int;
+            //param.Value = Id;
+            //ExecuteDataSet
+            DataSet dd=SqlHelper.ExecuteDataset(ConfigurationManager.AppSettings["ConnectionString"], CommandType.StoredProcedure, "GetEmployee");
+            //DataSet ds = SqlHelper.ExecuteDataset(, CommandType.StoredProcedure, "GetEmployee", param);
+
+        }
+
+    }
+}
---
+++ InventoryManagement.API/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement.API.Models
+{
+    public class Employee
+    {
+        public long EmpId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string Description { get; set; }
+    }
+}
